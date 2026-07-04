b7f73b8f285f64afab35e0dfcb135424a0241091
Logavelbalaji A K <logavelbalaji.ak@gmail.com>
2026-07-04 11:43:57 +0530

    Ex2

---
+++ Week1/JUnit, Mockito and SL4J/2JUnit_AdvancedTesting/Ex2_TestSuits/Code/FirstTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstTest {
+    @Test
+    public void testOne() {
+        assertTrue(true);
+    }
+}
---
+++ Week1/JUnit, Mockito and SL4J/2JUnit_AdvancedTesting/Ex2_TestSuits/Code/SecondTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecondTest {
+    @Test
+    public void testTwo() {
+        assertEquals(4, 2 + 2);
+    }
+}
