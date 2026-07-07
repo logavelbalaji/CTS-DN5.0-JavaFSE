23fbc18abb520484e1e1c53c33c1d73f0afd905c
Logavelbalaji A K <logavelbalaji.ak@gmail.com>
2026-07-07 22:50:23 +0530

    Ex6

---
+++ Week2/SpringDataJPA_Hibernate/Ex6_Pagination_Sorting/Code/Department.java
@@ -0,0 +1,28 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Employee> employees;
+}
